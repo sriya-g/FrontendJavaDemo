f48f15eee196f4c22324b3dea0a62a37e143aa94
sriya-g <75285747+sriya-g@users.noreply.github.com>
2025-02-18 20:18:17 -0600

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ var factText = document.getElementById("fact");
 const date = new Date();
 function displayFunFact() {
     factText.innerHTML = "Today is "+date;
+    if (myFavoriteNumber == 0) {
+        factText.innerHTML = "My favorite number is 0";
+    }
 }
 
 //Declaring Variables
@@ -13,15 +16,15 @@ let numGeese = 500;
 const NUM_DAYS_IN_WEEK = 7;
 //NUM_DAYS_IN_WEEK = 8; //doesn't work
 
-numGeese = "Millions";
 
-while (myFavoriteNumber > 0) {
-    myFavoriteNumber = myFavoriteNumber - 1;
-}
-//Display Button Values
+//Display Variable Values
 var dispButton = document.getElementById("var-disp");
-demoButton.addEventListener("click", updateVars);
+dispButton.addEventListener("click", updateVars);
 var disp = document.getElementById("disp");
-function displayFunFact() {
-    disp.innerHTML = '<h3>Favorite Number: '+myFavoriteNumber+'\n Geese on quad: '+numGeese+'\n Days in a week: '+NUM_DAYS_IN_WEEK+'</h3>';
+function updateVars() {
+    disp.innerHTML = '<h3>Favorite Number: '+myFavoriteNumber+'<br/> Geese on quad: '+numGeese+'<br/> Days in a week: '+NUM_DAYS_IN_WEEK+'</h3>';
+    numGeese = "Millions";
+    while (myFavoriteNumber > 0) {
+        myFavoriteNumber = myFavoriteNumber - 1;
+    }
 }
